8c31a2b0fa71776201bc1dd3eca12d9605d32d15
GentleIT <nasyrovrahat28@gmail.com>
2025-04-22 18:04:23 +0500

    Update main.go
    - Добавил Addequipment handler
    - Также добавил базу данных.
    
    Помимо этого были наверное еще некоторые правки

--- src/go/main.go
+++ src/go/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
-	// _ "github.com/lib/pq"
+
+	_ "github.com/lib/pq"
 )
 
 type Auth struct {
@@ -13,7 +15,37 @@ type Auth struct {
 }
 
 type token struct {
-	Token int // `json:"token"` // Не уверен что здесь нужно добавлять тег. Я ведь отправляю а не читаю
+	Token int `json:"token"` // Не уверен что здесь нужно добавлять тег. Я ведь отправляю а не читаю
+}
+
+type Equipment struct {
+	ID     int    `json:"id"`
+	Name   string `json:"name"`
+	Driver string `json:"driver"`
+}
+
+var db *sql.DB // Эту практику...
+
+func main() {
+	var err error // И эту практику подсмотрел из одного примера.
+	connStr := "user=postgres password=пароль123 dbname=postgres sslmode=disable"
+
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Println("Не удалось подключиться к базе postgres")
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Println(err)
+	} else {
+		log.Println("Подключение с базой данных установлено")
+	}
+
+	http.HandleFunc("/authorize", Authorization)
+	http.HandleFunc("/equipment", AddEquipment)
+	http.ListenAndServe("localhost:8080", nil)
 }
 
 func Authorization(w http.ResponseWriter, r *http.Request) {
@@ -33,11 +65,6 @@ func Authorization(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func main() {
-	http.HandleFunc("/authorize", Authorization)
-	http.ListenAndServe("localhost:8080", nil)
-}
-
 func SignUpCheck(log string, pass string) bool { // Это должно работать через базу данных
 	if log == "Admin" && pass == "12345" {
 		return true
@@ -45,10 +72,31 @@ func SignUpCheck(log string, pass string) bool { // Это должно рабо
 	return false
 }
 
+func AddEquipment(w http.ResponseWriter, r *http.Request) {
+	var newEquipment Equipment
+
+	if r.Method == http.MethodPost && r.Header.Get("token") == "123456789" {
+		w.Write([]byte("Succesfully entered to AddEquipment"))
+		err := json.NewDecoder(r.Body).Decode(&newEquipment)
+		if err != nil {
+			log.Println(err)
+		}
+
+		db.Exec("INSERT INTO equipment (name, driver) values ($1, $2)", newEquipment.Name, newEquipment.Driver)
+	} else {
+		w.Write([]byte("400"))
+	}
+}
+
 /*
+--- Начало
 	- Нужно чтоб через постмэн отправился логин и пароль на /login
 	- Сервер проверил данные
 	- Ответил Входом или не входом сообщением ок не ок.
 	- После этого загорается флаг isAuthinticated и можно использовать что-то далее.
 	= ВСЁ ЭТА КОММУНИКАЦИЯ ПРОХОДИТ ЧЕРЕЗ JSON.
+--- Законечено
+	- Добавить новый /equipment который принимает JSON с нужными полями для техники
+	- Добавить базу данных Postgres для сохранения добавленных данных в ней (нужно отправку реализовать)
+	- Доступ к эндпоинту /equipment должен проходить через токен
 */
